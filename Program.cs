a811a1d016a6a4d8f854d64e5f95c99c2c329ee8
Roman <foxllir@gmail.com>
2022-12-09 18:25:43 +0300

    dz_29

--- Program.cs
+++ Program.cs
@@ -1,23 +1,10 @@
 ﻿/*
-Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
-452 -> 11
-
-82 -> 10
-
-9012 -> 12
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 */
 
-int a;
-Console.Write($"Введите число: ");
-int.TryParse(Console.ReadLine(), out a);
-
-int b = a;
-int sum = 0;
-
-while (b > 0)
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
 {
-    sum += b % 10;
-    b /= 10;
+    array[i] = new Random { }.Next(10);
+    Console.Write($"{array[i]} ");
 }
-Console.Write($"{a} -> {sum}");
