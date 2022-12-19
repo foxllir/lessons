b259b338890ba5638d64e4e67c5e7eb460475459
Roman <foxllir@gmail.com>
2022-12-19 16:32:30 +0300

    dz_52

--- Program.cs
+++ Program.cs
@@ -1,36 +1,28 @@
 ﻿/*
-Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
 Например, задан массив:
-
 1 4 7 2
-
 5 9 2 3
-
 8 4 2 4
-
-17 -> такого числа в массиве нет
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
 
-int[,] array = new int[3, 3];
+int[,] array = new int[3, 4] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
 
 for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = new Random().Next(10);
         Console.Write($"{array[i, j]} ");
     }
+    Console.WriteLine();
 }
-Console.WriteLine();
 
-int iu, ju;
-Console.Write($"Введите i: ");
-int.TryParse(Console.ReadLine(), out iu);
-Console.Write($"Введите j: ");
-int.TryParse(Console.ReadLine(), out ju);
-
-if (iu >= 0 && iu < array.GetLength(0) && ju >= 0 && ju < array.GetLength(1))
-    Console.Write($"[{iu},{ju}] = {array[iu, ju]}");
-else
-    Console.Write($"Неверный ввод индексов");
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+        sum += array[i, j];
+    Console.Write($"{Math.Round((double)sum / array.GetLength(0), 1)} ");
+}
