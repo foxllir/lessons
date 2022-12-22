5df898a83147886f3c3cc696d45932a219557557
Roman <foxllir@gmail.com>
2022-12-22 17:51:05 +0300

    dz_41

--- Program.cs
+++ Program.cs
@@ -1,39 +1,23 @@
 ﻿/*
-Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-Массив размером 2 x 2 x 2
-66(0,0,0) 25(0,1,0)
-34(1,0,0) 41(1,1,0)
-27(0,0,1) 90(0,1,1)
-26(1,0,1) 55(1,1,1)
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
 */
 
-int[,,] a = new int[2, 2, 2];
+string[] numbers = Console.ReadLine()!.Split(' ');
+int[] array = new int[numbers.Length];
 
-int[] nums = new int[90];
-for (int i = 0; i < 90; i++)
-    nums[i] = i + 10;
-
-for (int i = 0; i < a.GetLength(0); i++)
+for (int i = 0; i < numbers.Length; i++)
 {
-    for (int j = 0; j < a.GetLength(1); j++)
-    {
-        for (int l = 0; l < a.GetLength(2); l++)
-        {
-            int temp = new Random().Next(90);
-
-            while (nums[temp] == 0)
-                temp = new Random().Next(90);
-
-            a[i, j, l] = nums[temp];
-            nums[temp] = 0;
-        }
-    }
+    int.TryParse(numbers[i], out array[i]);
+    Console.Write($"{array[i]} ");
 }
 
-for (int i = 0; i < a.GetLength(0); i++)
-    for (int j = 0; j < a.GetLength(1); j++)
-    {
-        for (int l = 0; l < a.GetLength(2); l++)
-            Console.Write($"{a[i, j, l]}({i}, {j}, {l}) ");
-        Console.WriteLine();
-    }
+int k = 0;
+foreach (var i in array)
+    if (i > 0)
+        k++;
+
+Console.Write($"\n{k} чисел больше 0");
